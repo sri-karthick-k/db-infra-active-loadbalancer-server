4fe970924ce62809d3948ef3f628a7651eb825cf
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2024-05-07 20:56:31 +0530

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { exec } = require('child_process');
+const cors = require("cors");
+require("dotenv").config()
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const port = 4000;
+
+app.get('/api/isActive', (req, res) => {
+  const virtualIp = process.env.VIRTUAL_IP;
+  const command = `ifconfig | grep ${virtualIp}`;
+  const command2 = "whoami";
+
+  // Object to hold the results of both commands
+  const results = {};
+
+  exec(command, (error, stdout, stderr) => {
+    if (!error) {
+      const isActive = stdout.trim().length > 0;
+      results.isActive = isActive.toString();
+    } else {
+      console.error('Error executing command:', error);
+      results.isActive = false;
+    }
+
+    // Execute the second command
+    exec(command2, (error2, stdout2, stderr2) => {
+      if (!error2) {
+        results.username = stdout2.trim();
+      } else {
+        console.error('Error executing command:', error2);
+        results.username = 'Unknown';
+      }
+
+      // Send the results object as JSON
+      res.json(results);
+    });
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
